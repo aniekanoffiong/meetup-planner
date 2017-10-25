ff57b1f2a81526d76e1521efb1d675c1e45af0e1
damomos <damomos123@gmail.com>
2017-10-25 16:30:39 +0100

    added about page

---
+++ about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us</title>
+	<link rel="stylesheet" type="text/css" href="about.css">
+</head>
+<body>
+
+<div class="container">
+	<div id="about">
+		Meetup planer is a money management  platform for community organizer. The platform gives them control and flexibility to customize their budget as needed, and delivers insightful actionable advice to help plan an event.
+	</div>
+</div>
+
+</body>
+</html>
